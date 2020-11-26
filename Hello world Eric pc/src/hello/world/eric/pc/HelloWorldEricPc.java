9f63022475ce7bb17a036a01fd7d3cb33881e97b
Eric Mabe <73889661+erickiimm@users.noreply.github.com>
2020-11-26 20:57:16 +0000

    Commit Eric Hello pc

---
+++ Hello world Eric pc/src/hello/world/eric/pc/HelloWorldEricPc.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.world.eric.pc;
+
+/**
+ *
+ * @author Eric Mabe 
+ * 2020366
+ */
+public class HelloWorldEricPc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello world CA3 Time and task");
+    }
+    
+}
